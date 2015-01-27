821db2af7b50ab5bb18d3c21e6fa6e4c94559aab
Nicholas J <nicholas.a.johns5@gmail.com>
2015-01-27 07:18:07 -0500

    Adjusted CI and added gitattributes to not download test files during checkout

--- tests/ci/scripts/install_protobuf.sh
+++ tests/ci/scripts/install_protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-wget "https://code.google.com/p/protobuf/downloads/detail?name=protobuf-2.5.0.zip"
+wget "https://code.google.com/p/protobuf/downloads/detail?name=protobuf-2.5.0.zip&can=2&q=" -O "protobuf-2.5.0.zip"
 unzip protobuf-2.5.0.zip
 cd protobuf-2.5.0 && ./configure --prefix=/usr && make && sudo make install
 cd $HOME
