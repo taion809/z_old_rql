504b088e4cc7d3190d4e0aa85c66d8cdd9c88480
Nicholas J <nicholas.a.johns5@gmail.com>
2015-01-27 07:22:53 -0500

    Adjusted CI

--- tests/ci/scripts/install_php_protobuf.sh
+++ tests/ci/scripts/install_php_protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-wget "https://github.com/chobie/php-protocolbuffers/archive/master.zip" -O "php-protobuf.zip"
+wget -L "https://github.com/chobie/php-protocolbuffers/archive/master.zip" -O "php-protobuf.zip"
 unzip php-protobuf.zip
 cd php-protocolbuffers-master && phpize && ./configure --prefix=/usr && make && sudo make install
 cd $HOME
