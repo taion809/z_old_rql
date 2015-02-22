ee1cb5af2c722f8842df8ce2b519f67d90d1b1ba
Nicholas J <nicholas.a.johns5@gmail.com>
2015-02-22 22:58:39 +0000

    Updated queries, including new ones still NYI

---
+++ src/Rql/Query/Geospacial/Polygon.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: njohns
+ * Date: 2/22/15
+ * Time: 5:53 PM
+ */
+
+namespace Rql\Query\Geospacial;
+
+use Rql\Datum;
+use Rql\Generated\Term\TermType;
+use Rql\Query\Query;
+
+class Polygon extends Query
+{
+    public function __construct(Query $sequence)
+    {
+        throw new \RuntimeException("Not Yet Implemented");
+
+        $this->terms[] = $sequence;
+    }
+
+    protected function getDatumType()
+    {
+        return TermType::POLYGON;
+    }
+}
