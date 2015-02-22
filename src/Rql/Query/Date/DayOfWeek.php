bd8448ed9b60346b53e255207ceaff933266a8cb
Nicholas J <nicholas.a.johns5@gmail.com>
2015-02-22 23:04:33 +0000

    Updated queries, including new ones still NYI

---
+++ src/Rql/Query/Date/DayOfWeek.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: njohns
+ * Date: 2/22/15
+ * Time: 5:59 PM
+ */
+
+namespace Rql\Query\Date;
+
+use Rql\Datum;
+use Rql\Generated\Term\TermType;
+use Rql\Query\Query;
+
+class DayOfWeek extends Query
+{
+    public function __construct(Query $sequence)
+    {
+        throw new \RuntimeException("Not Yet Implemented");
+
+        $this->terms[] = $sequence;
+    }
+
+    protected function getDatumType()
+    {
+        return TermType::DAY_OF_WEEK;
+    }
+}
